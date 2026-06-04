edb3944e58bade5ce4e1bb8063122a2868fe563a
BryanBarrozo <bryanbarrozox2@gmail.com>
2026-06-04 16:51:41 -0300

    head responsiviness done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,22 +1,63 @@
 import styles from './Header.module.css'
+import { useState, useEffect } from 'react'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { 
+import {
     faMagnifyingGlass,
     faUser,
-    faBagShopping
- } from '@fortawesome/free-solid-svg-icons'
+    faBagShopping,
+    faBars
+} from '@fortawesome/free-solid-svg-icons'
 
-function Header(){
-    return(
+function Header() {
+
+    const [menu, setMenu] = useState(false)
+
+    useEffect(() => {
+
+    function handleResize() {
+        if(window.innerWidth > 1150){setMenu(false)}
+    }
+
+    window.addEventListener("resize", handleResize)
+
+    return () => {
+        window.removeEventListener("resize", handleResize)
+    }
+
+}, [])
+
+    return (
         <header className={styles.head}>
-            <nav className={styles.navBar}>{/*trocar pelo Route*/}
+
+            <button
+                type="button"
+                className={styles.menuButton}
+                onClick={() => setMenu(!menu)}
+            >
+                <FontAwesomeIcon icon={faBars} />
+            </button>
+
+            <nav
+                className={`${styles.toggleNavBar} ${
+                    menu ? styles.open : ''
+                }`}
+            >
                 <a href="">New Arrivals</a>
                 <a href="">Collection</a>
                 <a href="">Objects</a>
                 <a href="">Journal</a>
                 <a href="">About</a>
             </nav>
+
+            <nav className={styles.navBar}>
+                <a href="">New Arrivals</a>
+                <a href="">Collection</a>
+                <a href="">Objects</a>
+                <a href="">Journal</a>
+                <a href="">About</a>
+            </nav>
+
             <h1 className={styles.title}>AESTHETE</h1>
 
             <div className={styles.menu}>
@@ -24,6 +65,7 @@ function Header(){
                 <FontAwesomeIcon icon={faUser} />
                 <FontAwesomeIcon icon={faBagShopping} />
             </div>
+
         </header>
     )
 }
